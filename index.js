e0ed63b791916923c93235573efa747442deac1e
Andrew Stonecipher <andrew.stonecipher@hotmail.com>
2020-04-24 15:58:12 -0500

    shit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send(process.env.env_name);
+  res.send('Something');//process.env.env_name);
 });
-app.listen(80, function () {
+// const port = 3000;
+app.listen(5000, function () {
   console.log('Example app listening on port 3000!');
 });
