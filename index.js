ed74b2c84fcda8a69b96a0d9e947a0f3fe21b002
Andrew Stonecipher <andrew.stonecipher@hotmail.com>
2020-04-24 16:59:23 -0500

    updated paths

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Something');//process.env.env_name);
 });
+app.get('/helo', function (req, res) {
+  res.send((process.env.env_name|| 'Not Set'));
+});
 // const port = 3000;
 app.listen(5050, function () {
   console.log('Example app listening on port 3000!');
